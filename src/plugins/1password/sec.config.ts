fcde0df8c4b94eb8883dfc5b1db6254d2597dbe5
Mitchell R <support@bettercorp.co.za>
2022-08-09 23:38:25 +0200

    Added plugin config definition

--- src/plugins/1password/sec.config.ts
+++ src/plugins/1password/sec.config.ts
@@ -1,16 +1,19 @@
 export interface MyPluginConfig {
-  serverUrl: string;
-  token: string;
-  vaultId: string;
-  lockToVault: boolean;
+  serverUrl: string; // Server URL: The server URL for the 1password connect service
+  token: string; // Token
+  vaultId: string; // Vault ID
+  lockToVault: boolean; // Vault lock: Lock access to a specific vault ID
 }
 
-export default (pluginName: string, existingPluginConfig: any): MyPluginConfig => {
+export default (
+  pluginName: string,
+  existingPluginConfig: any
+): MyPluginConfig => {
   let newConfig: MyPluginConfig = {
-    serverUrl: 'http://localhost',
-    token: 'localhost',
-    vaultId: 'localhost',
-    lockToVault: true
+    serverUrl: "http://localhost",
+    token: "localhost",
+    vaultId: "localhost",
+    lockToVault: true,
   };
   return newConfig;
 };
